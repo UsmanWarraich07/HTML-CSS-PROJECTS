e5a332b67138fc065aeade84734e37d5efabf397
UsmanWarraich07 <musmanwarraich238@gmail.com>
2024-04-29 21:26:56 +0500

    update e-commerance site

--- 05-Ecommerance Site/script.js
+++ 05-Ecommerance Site/script.js
@@ -1,10 +1,15 @@
 const toggleBtn = document.getElementById("toggle");
 const navLinks = document.getElementById("nav-links");
-const closeBtn = document.getElementById("close")
-toggleBtn.addEventListener('click', () => {
-  navLinks.style.display = 'block';
+const closeBtn = document.getElementById("close");
+const year = document.getElementById("year");
+
+const getYear = new Date().getFullYear();
+year.innerText = getYear;
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.style.display = "block";
 });
 
-closeBtn.addEventListener('click', () => {
-  navLinks.style.display = 'none';
-})
+closeBtn.addEventListener("click", () => {
+  navLinks.style.display = "none";
+});
